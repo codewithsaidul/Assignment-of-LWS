0cd6f66fefbf6d17c099be0eda15f18f43438ddc
codewithsaidul <codewithsaidul@gmail.com>
2025-03-21 01:14:26 +0600

    html to react convert complete on assignment 4

--- Book Store Assignment4 - LWS/src/App.jsx
+++ Book Store Assignment4 - LWS/src/App.jsx
@@ -1,9 +1,20 @@
+import AddBook from "./components/AddBook"
+import BookList from "./components/BookList"
+import Navbar from "./components/Navbar"
 
 
 function App() {
 
   return (
-    <div className="text-5xl text-rose-500">hello</div>
+    <div>
+      <Navbar />
+      <main className="py-12 2xl:px-6">
+        <div className="container grid xl:grid-cols-[auto_350px] 2xl:grid-cols-[auto_400px] gap-4 2xl:gap-8">
+          <BookList />
+          <AddBook />
+        </div>
+      </main>
+    </div>
   )
 }
 
